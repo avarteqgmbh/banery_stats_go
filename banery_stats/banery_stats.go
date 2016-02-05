8a9d1cd1593484ffdc8d7cc9859847a6807710eb
Robert Gogolok <gogolok@gmail.com>
2016-02-05 23:27:44 +0100

    mv workspace outputting to separate func

--- banery_stats/banery_stats.go
+++ banery_stats/banery_stats.go
@@ -8,37 +8,40 @@ import (
 func Run() {
 	logger := Logger()
 
+	if len(ApiToken()) == 0 {
+		logger("Set KANBANERY_API_TOKEN to your personal kanbanery API token")
+		return
+	}
 
-  if len(ApiToken()) == 0 {
-    logger("Set KANBANERY_API_TOKEN to your personal kanbanery API token")
-    return
-  }
-
-  workspaceClient := ClientWithKeyForWorkspaceApi(ApiToken())
+	workspaceClient := ClientWithKeyForWorkspaceApi(ApiToken())
 	workspaces := GetWorkspaces(workspaceClient)
-  if len(workspaces) == 0 {
-    logger("No workspaces present.")
-    return
-  }
+	if len(workspaces) == 0 {
+		logger("No workspaces present.")
+		return
+	}
 
-
-  ownUserIdClient := ClientWithKeyAndWorkspaceName(ApiToken(), workspaces[0].Name)
-  ownUserId := GetOwnUserId(ownUserIdClient)
+	ownUserIdClient := ClientWithKeyAndWorkspaceName(ApiToken(), workspaces[0].Name)
+	ownUserId := GetOwnUserId(ownUserIdClient)
 
 	for _, workspace := range workspaces {
-		logger("WORKSPACE " + workspace.Name + " #projects: " + fmt.Sprintf("%v", len(workspace.Projects)))
-
-    client := ClientWithKeyAndWorkspaceName(ApiToken(), workspace.Name)
-		for _, project := range workspace.Projects {
-			project_tasks := GetOwnProjectTasks(client, project.Id, ownUserId)
-			project_tasks_length := len(project_tasks)
-			if project_tasks_length > 0 {
-				logger(fmt.Sprintf("%5v", project_tasks_length) + " " + project.Name)
-			}
-		}
+		outputWorkspace(logger, workspace, ownUserId)
 	}
 }
 
 func ApiToken() string {
 	return os.Getenv("KANBANERY_API_TOKEN")
+}
+
+func outputWorkspace(logger func(string), workspace Workspace, ownUserId OwnId) {
+	logger("WORKSPACE " + workspace.Name + " #projects: " + fmt.Sprintf("%v", len(workspace.Projects)))
+
+	client := ClientWithKeyAndWorkspaceName(ApiToken(), workspace.Name)
+	for _, project := range workspace.Projects {
+		project_tasks := GetOwnProjectTasks(client, project.Id, ownUserId)
+		project_tasks_length := len(project_tasks)
+		if project_tasks_length > 0 {
+			logger(fmt.Sprintf("%5v", project_tasks_length) + " " + project.Name)
+		}
+	}
+
 }
